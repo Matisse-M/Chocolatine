c3aee1df12f69fee578f0ee64f5b208aeb7b85f3
Matisse-M <matisse.marsac@gmail.com>
2024-03-18 11:14:40 +0100

    [UPDATE] Project

--- Makefile
+++ Makefile
@@ -1,77 +1,100 @@
 ##
-## EPITECH PROJECT, 2023
-## Makefile
+## EPITECH PROJECT, 2024
+## B-CPE-110-NAN-1-1-secured-albane.merian
 ## File description:
-## Makefile to compil the -lmy
+## Makefile
 ##
 
-SRC		=	main.c					\
-			src/structure.c 		\
-			src/fill_structure.c	\
-			src/toolbar.c			\
-			src/hover_toolbar.c		\
-			src/scenes_toolbar.c	\
-			src/save.c				\
-			src/drawing.c			\
-			src/my_paint.c			\
-			src/paint_two.c			\
-			src/paint_three.c		\
-			src/exit_menu.c			\
-			src/shader.c			\
-			src/rename.c			\
-			src/alphabet.c			\
-			src/my_alpha.c 			\
-			src/my_alpha_maj.c 		\
-			src/my_num.c			\
-			src/opening.c			\
-			src/open_struct.c		\
-			src/help_menu.c			\
-			src/file_btn.c 	   		\
-			src/edit_btn.c 	   		\
-			src/help_btn.c 	   		\
-			src/destroys.c			\
-			src/about.c				\
-			src/layers.c			\
-			src/shapes.c			\
-			src/stamps.c			\
-			src/get_files.c
+CC	=	gcc
 
-NAME	= 	my_paint
+NAME	=	secured
 
-INCLUDE =	-I ./include
+UNIT_TEST = test
 
-LIB 	=	-L ./lib -lcsfml -L ./lib -lmy
+CFLAGS	=	-g -Wshadow -Werror -I./include
 
-CSFML	=	-lcsfml-window -lcsfml-graphics -lcsfml-system -lcsfml-audio \
-			-lcsfml-network -lm
+CRIT_FLAG	=	--coverage -lcriterion
 
-all:	$(NAME)
+LIBS	=	-L ./ -lhashtable
 
-$(NAME):
-		@make -C ./lib/csfml
-		@make -C ./lib/my
-		@cp -f ./lib/csfml/csfml.h ./include
-		@cp -f ./lib/my/my.h ./include
-		@gcc -g -o $(NAME) $(SRC) $(INCLUDE) $(LIB) $(CSFML)
-		@echo -e "\e[1;36m{ Binary ready !! }\e[00;37m"
+SRC		=   main.c	\
+
+SRC_TEST =	hash_lib/my_strlen.c		\
+			hash_lib/my_getnbr.c	\
+			hash_lib/my_put_nbr.c	\
+			hash_lib/my_strdup.c		\
+			hash_lib/my_positive_nbr.c \
+			hash_lib/mini_printf.c 		\
+			hash_lib/pf.c 				\
+			hash_lib/my_intlen.c 		\
+			hash_lib/my_putchar.c	\
+			hash_lib/my_putstr.c		\
+			hash_lib/my_strcpy.c		\
+			hash_lib/my_strcmp.c			\
+			hash_lib/my_unsigned_nbr.c	\
+			hash_lib/my_compute_power_rec.c 	\
+			hash_lib/delete_key.c	\
+			hash_lib/delete.c	\
+			hash_lib/dump.c	\
+			hash_lib/hash.c		\
+			hash_lib/insert.c	\
+			hash_lib/new_hash.c	\
+			hash_lib/search.c
+
+TEST	= 	tests/test_squared.c		\
+			tests/test_getnbr.c			\
+			tests/test_positive.c		\
+			tests/test_strdup.c 		\
+			tests/test_strcpy.c			\
+			tests/test_strcmp.c			\
+			tests/test_destroy.c		\
+			tests/test_dump.c			\
+			tests/test_insert.c			\
+			tests/test_search.c			\
+			tests/test_printf.c			\
+			tests/test_number.c			\
+			tests/test_delete.c
+
+OBJ		=	$(SRC:.c=.o)
+
+all:	libmaking $(NAME)
+
+$(NAME):	$(OBJ)
+			$(CC) -o $(NAME) $(OBJ) $(CFLAGS) $(LIBS) -I ./include
+			@echo -e "\e[1;32m{ Makefile is ready }\e[00;37m"
+			@echo -e "\e[1;36m{ Binary is ready }\e[00;37m"
+
+
+.PHONY: libmaking
+
+libmaking:
+			$(MAKE) -C hash_lib
+
+$(UNIT_TEST): fclean all
+	$(CC) -o $(UNIT_TEST) $(SRC_TEST) $(TEST) $(CRIT_FLAG) $(LIBS)
+
+tests_run: $(UNIT_TEST)
+		@./$(UNIT_TEST)
+		@gcovr --exclude tests/
 
 clean:
-		@rm -f *~
-		@make clean -C ./lib/csfml
-		@make clean -C ./lib/my
-		@echo -e "\e[1;33m[ Useless files have been deleted ]\e[00;37m"
+			$(MAKE) -C hash_lib clean
+			rm -f $(OBJ)
+			rm -f *.gc*
+			rm -f coding-style-reports.log
+			rm -f *~
+			rm -f *.gcda
+			rm -f *.gcno
+			rm -f secured
+			rm -f unit_tests
+			rm -f libhashtable.a
+			rm -f test
 
-fclean: clean
-		@rm -f $(NAME)
-		@rm -rf lib/libcsfml.a lib/libmy.a
-		@make fclean -C ./lib/csfml
-		@make fclean -C ./lib/my
-		@echo -e "\e[1;33m[ Binary has been deleted ]\e[00;37m"
+fclean:	clean
+			$(MAKE) -C hash_lib fclean
+			@rm -f $(NAME)
+			@rm -f $(UNIT_TEST)
+			@echo -e "\e[1;33m{ REPOSITORIE as been cleaned }\e[00;37m"
 
-norm:
-	@make clean
-	@make fclean
-	@coding-style . .
-	@cat coding-style-reports.log
 
-re: fclean all
+re:	fclean all
--- main.c
+++ main.c
@@ -1,114 +1,81 @@
 /*
-** EPITECH PROJECT, 2023
-** main
+** EPITECH PROJECT, 2024
+** B-CPE-110-NAN-1-1-secured-albane.merian
 ** File description:
-** main()
+** main
 */
 
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdarg.h>
-#include "paint.h"
-#include "my.h"
+#include "include/hashtable.h"
+#include "include/my.h"
 
-static int check_exist_four(char **av, int stat)
+hashtable_t *test_2(hashtable_t *ht)
 {
-    sfTexture *shape = NULL;
-
-    shape = sfTexture_createFromFile("assets/shape.png", NULL);
-    if (!shape)
-        return 84;
-    sfTexture_destroy(shape);
-    stat = open("assets/help_script.txt", O_RDONLY);
-    if (stat == -1) {
-        write(2, "File doesn't exist or is misplaced\n", 36);
-        return 84;
-    }
+    ht_insert(ht, NULL, "defe");
+    mini_printf("both empty \n");
+    ht_insert(ht, "", "");
+    mini_printf("both null\n");
+    ht_insert(ht, NULL, NULL);
+    ht_search(ht, NULL);
+    ht_search(ht, "");
+    ht_delete(ht, "02");
+    ht_delete(ht, "03");
+    ht_delete(ht, "03");
+    printf("\nLooking for \"%s\":\"%s\"\n", " 05",
+        ht_search(ht, "05"));
+    printf("\nLooking for \"%s\":\"%s\"\n", " 02",
+        ht_search(ht, "02"));
+    ht_dump(ht);
+    mini_printf("\n deleting inexistant node\n");
+    ht_delete(ht, "Fun");
+    delete_hashtable(ht);
+    ht_dump(ht);
+    return (ht);
 }
 
-int check_exist_three(char **av, int stat)
+hashtable_t *test(hashtable_t *ht)
 {
-    sfTexture *up = NULL;
-    sfFont *font = NULL;
-
-    up = sfTexture_createFromFile("assets/up.png", NULL);
-    if (!up) {
-        sfTexture_destroy(up);
-        return 84;
-    }
-    sfTexture_destroy(up);
-    font = sfFont_createFromFile("assets/font.ttf");
-    if (!font) {
-        sfFont_destroy(font);
-        return 84;
-    }
-    sfFont_destroy(font);
-    if (check_exist_four(av, stat) == 84)
-        return 84;
+    ht_dump(ht);
+    ht_insert(ht, "05", "bonjour");
+    ht_insert(ht, "04", "fun");
+    mini_printf("\nFilled :\n");
+    ht_dump(ht);
+    mini_printf("\n");
+    mini_printf("Looking for \"%s \n\n", "Kra");
+    ht_search(ht, "Kra");
+    ht_search(ht, "edefe");
+    ht_delete(ht, NULL);
+    mini_printf("Visions && kratos was deleted :)\n");
+    ht_delete(ht, "01");
+    ht_dump(ht);
+    mini_printf("\none empty string\n");
+    ht_insert(ht, "fd", "");
+    mini_printf("one null arg\n");
+    ht_insert(ht, "ezf", NULL);
+    mini_printf("key null\n");
+    test_2(ht);
+    return (ht);
 }
 
-int check_exist_two(char **av)
+int main(int argc, char **argv)
 {
-    sfTexture *up_h = NULL;
-    sfTexture *pipette = NULL;
-    int stat = 0;
+    hashtable_t *ht = NULL;
 
-    pipette = sfTexture_createFromFile("assets/pipette.png", NULL);
-    if (!pipette) {
-        sfTexture_destroy(pipette);
+    if (argc > 1)
         return 84;
-    }
-    sfTexture_destroy(pipette);
-    up_h = sfTexture_createFromFile("assets/up_h.png", NULL);
-    if (!up_h) {
-        sfTexture_destroy(up_h);
-        return 84;
-    }
-    sfTexture_destroy(up_h);
-    if (check_exist_three(av, stat) == 84)
-        return 84;
-}
-
-int check_if_sprite_exist(char **av)
-{
-    sfTexture *down_h = sfTexture_createFromFile("assets/down_h.png", NULL);
-    sfTexture *downe = NULL;
-
-    if (!down_h) {
-        sfTexture_destroy(down_h);
-        return 84;
-    }
-    sfTexture_destroy(down_h);
-    downe = sfTexture_createFromFile("assets/down.png", NULL);
-    if (!downe) {
-        sfTexture_destroy(downe);
-        return 84;
-    }
-    sfTexture_destroy(downe);
-    if (check_exist_two(av) == 84)
-        return 84;
-}
-
-static char *get_env(char **env)
-{
-    char *user = NULL;
-
-    for (int i = 0; env[i] != NULL; i++) {
-        if (my_strncmp(env[i], "USER=", 5) == 0)
-            user = my_strdup(&env[i][5]);
-    }
-    return user;
-}
-
-int main(int ac, char **av, char **env)
-{
-    sfEvent event;
-    char *user = get_env(env);
-
-    if (check_if_sprite_exist(av) == 84)
-        return 84;
-    my_paint(event, user);
-    return (0);
+    delete_hashtable(ht);
+    ht_dump(ht);
+    ht_insert(ht, NULL, NULL);
+    ht_search(ht, NULL);
+    ht_delete(ht, NULL);
+    hash("ferfe", -1);
+    ht = new_hashtable(&hash, 5);
+    mini_printf("\nEmpty :\n");
+    ht_dump(ht);
+    ht_insert(ht, "01", "./Documents/Tournament/Modules/Vision");
+    ht_insert(ht, "02", "./Trash/Hollidays_Pics/.secret/kratos.ai");
+    ht_insert(ht, "03", "+33 63145612371");
+    ht_insert(ht, "04", "theh enkdjer");
+    ht_insert(ht, "05", "dkjf fehtrkh");
+    test(ht);
+    return 0;
 }
